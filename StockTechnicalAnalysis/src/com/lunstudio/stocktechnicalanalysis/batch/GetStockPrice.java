f0beb408bacbfa32bcdd1cc43f64de7456f6a3ad
alankam <alankam@192.168.1.18>
2018-10-15 01:28:01 +0800

    First Source Commit

---
+++ StockTechnicalAnalysis/src/com/lunstudio/stocktechnicalanalysis/batch/GetStockPrice.java
@@ -0,0 +1,10 @@
+package com.lunstudio.stocktechnicalanalysis.batch;
+
+public class GetStockPrice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing");
+	}
+
+}
